a05088d74840f60d0a4333283eebd31443666e7a
Brayan-724 <bjqf0724@gmail.com>
2023-07-24 18:40:20 -0600

    refactor: Improve code quality and performance

--- src/commands/dev.rs
+++ src/commands/dev.rs
@@ -4,7 +4,6 @@ use std::{
     process,
     sync::{
         atomic::{AtomicBool, Ordering},
-        mpsc::{self, Receiver, SendError},
         Arc,
     },
 };
@@ -39,97 +38,48 @@ impl DevCommand {
         let cwd = std::env::current_dir().unwrap();
         let target_path: PathBuf = join_paths(folder, cwd).into();
 
-        let (shutdown_tx, shutdown_rx, shutdown_rx_main) = spmc_channel();
-        let shutdown_threads = move || shutdown_tx(());
-        let (watch_event_tx, watch_event_rx) = mpsc::channel();
-
         let watching_path = target_path.clone();
-        let watching = thread::spawn(move || {
-            let mut poll = PollWatcher::new(watching_path).unwrap();
-            poll.scheduling_poll(Duration::from_millis(500), shutdown_rx, |x| {
-                watch_event_tx.send(x).unwrap();
-            })
-        });
+        let mut watching_poll = PollWatcher::new(watching_path).unwrap();
 
-        let (first_build_tx, first_build_rx) = mpsc::sync_channel(1);
         let target_path_main = target_path.clone();
-        let main = thread::spawn(move || -> Option<()> {
-            let compile_context = CompileContext {
-                output_dir: join_paths(".densky", &target_path_main),
-                routes_path: join_paths("src/routes", &target_path_main),
-                views_path: join_paths("src/views", &target_path_main),
-                static_path: join_paths("src/static", &target_path_main),
-                verbose: true,
-                static_prefix: "static/".to_owned(),
-            };
 
-            let progress = progress::create_spinner(Some("Discovering"));
-
-            match write_aux_files(&compile_context) {
-                Ok(_) => (),
-                Err(e) => {
-                    let _ = first_build_tx.send(Result::Err(e));
-                    return None;
-                }
-            };
-            progress.tick();
-
-            let (http_container, http_tree) = http_discover(&compile_context).ok()?;
-            progress.tick();
-
-            Manifest::update(&http_container, &compile_context).unwrap();
-            progress.tick();
-
-            let views = view_discover(&compile_context);
-
-            progress.finish();
-            for view in views {
-                process_view(view);
-            }
-            let _ = first_build_tx.send(Ok(()));
-
-            println!(
-                "\x1B[2J\x1B[1;1H{}\n",
-                Fmt(|f| http_tree.lock().unwrap().display(f, &http_container))
-            );
-
-            '_loop: loop {
-                if let Ok(event) = watch_event_rx.recv_timeout(Duration::from_millis(10)) {
-                    let (http_container, http_tree) = http_discover(&compile_context).ok()?;
-                    Manifest::update(&http_container, &compile_context).unwrap();
-                    // for view in views {
-                    //     process_view(view);
-                    // }
-                    println!(
-                        "\x1B[2J\x1B[1;1H{}\n",
-                        Fmt(|f| http_tree.lock().unwrap().display(f, &http_container))
-                    );
-                    DevCommand::send_update(event.iter().map(|e| (e.kind.clone(), &e.path)));
-                }
-
-                if let Ok(_) = shutdown_rx_main.recv_timeout(Duration::from_millis(1)) {
-                    println!("[Main] Shutdown");
-                    break;
-                }
-            }
-
-            None
-        });
-
-        let shutdown_threads = move || {
-            (
-                shutdown_threads().unwrap(),
-                watching.join().unwrap(),
-                main.join().unwrap(),
-            )
+        let compile_context = CompileContext {
+            output_dir: join_paths(".densky", &target_path_main),
+            routes_path: join_paths("src/routes", &target_path_main),
+            views_path: join_paths("src/views", &target_path_main),
+            static_path: join_paths("src/static", &target_path_main),
+            verbose: true,
+            static_prefix: "static/".to_owned(),
         };
 
-        let first_build = first_build_rx.recv().unwrap();
-        if let Err(e) = first_build {
-            eprintln!("Error on first build: {e}");
-            let _ = shutdown_threads();
-            return;
+        let progress = progress::create_spinner(Some("Discovering"));
+
+        match write_aux_files(&compile_context) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error on first build: {e}");
+                return;
+            }
+        };
+        progress.tick();
+
+        let (http_container, http_tree) = http_discover(&compile_context);
+        progress.tick();
+
+        Manifest::update(&http_container, &compile_context).unwrap();
+        progress.tick();
+
+        let views = view_discover(&compile_context);
+
+        progress.finish();
+        for view in views {
+            process_view(view);
         }
+
+        println!(
+            "\x1B[2J\x1B[1;1H{}\n",
+            Fmt(|f| http_tree.lock().unwrap().display(f, &http_container))
+        );
 
         let mut deno = process::Command::new("deno")
             .args(["run", "-A"])
@@ -141,14 +91,45 @@ impl DevCommand {
         let sigint =
             signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&term)).unwrap();
 
-        // wait to interrupt
-        while !term.load(Ordering::Relaxed) {}
+        '_loop: loop {
+            DevCommand::handle_update(&compile_context, &mut watching_poll);
 
-        // TODO: Check memory leaks on this line
-        assert!(signal_hook::low_level::unregister(sigint));
+            // wait to interrupt
+            if term.load(Ordering::Relaxed) {
+                // TODO: Check memory leaks on this line
+                assert!(signal_hook::low_level::unregister(sigint));
+                let _ = deno.kill(); // Err(): Command wasn't running
+                return;
+            }
 
-        let _ = deno.kill();
-        let _ = shutdown_threads();
+            thread::sleep(Duration::from_millis(200));
+        }
+    }
+
+    fn handle_update(compile_context: &CompileContext, watching_poll: &mut PollWatcher) {
+        let event = watching_poll.poll();
+        if event.len() != 0 {
+            let (http_container, http_tree) = http_discover(&compile_context);
+            let views = view_discover(&compile_context);
+
+            println!(
+                "\x1B[2J\x1B[1;1H{}\n",
+                Fmt(|f| http_tree.lock().unwrap().display(f, &http_container))
+            );
+
+            for view in views {
+                process_view(view);
+            }
+
+            match Manifest::update(&http_container, &compile_context) {
+                Ok(_) => {}
+                Err(err) => {
+                    eprintln!("Error updating manifest: {err}")
+                }
+            }
+
+            DevCommand::send_update(event.iter().map(|e| (e.kind.clone(), &e.path)));
+        }
     }
 
     pub fn send_update<I, P>(files: I)
@@ -184,27 +165,4 @@ impl DevCommand {
             }
         }
     }
-}
-
-fn spmc_channel<T>() -> (
-    impl FnOnce(T) -> Result<(), SendError<T>>,
-    Receiver<T>,
-    Receiver<T>,
-)
-where
-    T: Clone,
-{
-    let (tx1, rx1) = mpsc::channel();
-    let (tx2, rx2) = mpsc::channel();
-
-    return (
-        move |v| {
-            tx1.send(v.clone())?;
-            tx2.send(v.clone())?;
-
-            Ok(())
-        },
-        rx1,
-        rx2,
-    );
 }
